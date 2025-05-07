9d3caf3f69f9fae74dee1070a44a0f964554849e
nomanoma121 <takeuchi180121@gmail.com>
2025-05-08 08:47:43 +0900

    feat: 参照元を取得できるように

---
+++ src/constant.ts
@@ -0,0 +1,3 @@
+export const PROMPT_PREFIX = "あなたは名探偵コナンの服部平治です。関西弁で話してください。";
+
+export const REFERENCE_PREFIX = "このメッセージは、次のメッセージに対して返信されたものです。";
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Client, GatewayIntentBits, Events } from "discord.js";
 import { getResponse } from "./api";
 import config from "../config.json";
+import { PROMPT_PREFIX, REFERENCE_PREFIX } from "./constant";
 
 const client = new Client({
   intents: [
@@ -15,11 +16,30 @@ client.once(Events.ClientReady, (readyClient) => {
 });
 
 client.on("messageCreate", async (message) => {
+  const trimMessage = (content: string) => {
+    const trimmedMessage = content.replace(/<@&?(\d+)>/, "").trim();
+    return trimmedMessage;
+  };
+
   if (message.author.bot) return;
   if (message.mentions.has(client.user)) {
+    // 返信の場合は参照元を取得
+    let referencedMessage = null;
+    if (message.reference) {
+      if (message.reference?.messageId) {
+        referencedMessage = await message.channel.messages.fetch(
+          message.reference.messageId
+        );
+      }
+    }
     console.log(`Mentioned by ${message.author.tag}`);
-    const trimmedMessage = message.content.replace(/<@&?(\d+)>/, "").trim();
-    const requestMessage = `あなたは名探偵コナンの服部平治です。関西弁で話してください。 ${trimmedMessage}`;
+    const trimmedMessage = trimMessage(message.content);
+    const requestMessage = `${PROMPT_PREFIX} ${trimmedMessage} ${
+      referencedMessage
+        ? REFERENCE_PREFIX + trimMessage(referencedMessage.content)
+        : ""
+    }`;
+    console.log(`Request message: ${requestMessage}`);
     const response = await getResponse(requestMessage);
     message.reply(response);
   }
